a003a20fa9b327d06f47325c83f394e3f177d67c
Helen Berhane <helen.berhane@powercoders.org>
2018-12-14 16:40:38 +0100

    My javascript

--- scratch/change-class/setClass.js
+++ scratch/change-class/setClass.js
@@ -4,17 +4,7 @@ function greet(name) {
   console.log('Hello, ' + name);
 }
 
-function setClassTip(event) {
+function setClass(event, className) {
   let el = document.getElementById('mypara');
-  el.className = 'tip';
-}
-
-function setClassWarning(event) {
-  let el  = document.getElementById('mypara');
-  el.className = 'warning';
-}
-
-function clearClass(event) {
-  let el  = document.getElementById('mypara');
-  el.className = '';
+  el.className = className;
 }
