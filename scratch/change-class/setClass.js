cf2a7df7ae18cd8884be7c7b1b01b4cc3e29e458
Helen Berhane <helen.berhane@powercoders.org>
2018-12-20 13:46:08 +0100

    function in change class

--- scratch/change-class/setClass.js
+++ scratch/change-class/setClass.js
@@ -1,10 +1,32 @@
-console.log('My code is running!');
+//console.log('My code is JS line');
 
-function greet(name) {
-  console.log('Hello, ' + name);
-}
-
-function setClass(event, className) {
+function setClass(className) {
+  //console.log('setClass is running');
   let el = document.getElementById('mypara');
   el.className = className;
 }
+
+//console.log('This is after setClass is defined');
+
+document.addEventListener('DOMContentLoaded', function(event){
+  //console.log('DOMContentLoaded has fired');
+  let makeWarning = document.getElementById('make-warning');
+  makeWarning.addEventListener('click', function(event){
+    //console.log('click listener fired');
+    setClass('warning');
+  });
+
+  let makeTip = document.getElementById('make-tip');
+  makeTip.addEventListener('click', function(event){
+    setClass('tip');
+  });
+
+  let makeNormal = document.getElementById('make-normal');
+  makeNormal.addEventListener('click', function(event){
+    setClass('');
+  });
+});
+
+
+
+
