318250db68a0be5fe379cf7ff09b8d13bf2705fe
Helen Berhane <helen.berhane@powercoders.org>
2019-01-10 14:39:01 +0100

    Allow the user to add list item by using Enter

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,10 +1,58 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('button').addEventListener('click', function(event){
-        console.log('The button was clicked');
+function createNewListItem(itemName) {
+  let listItem = document.createElement('li');
+  let span = document.createElement('span');
+  let button = document.createElement('button');
 
-     //with variable.
-     let inputBox = document.getElementById('item');
-     console.log(inputBox.value);
-    });
+  let textSpan = document.createTextNode(itemName);
+  let deleteButton = document.createTextNode('delete');
+
+  span.appendChild(textSpan);
+  button.appendChild(deleteButton);
+
+  //Add a click handler that logs the click here
+  button.addEventListener('click', function (event) {
+    console.log('Delete button is clicked: ' + itemName);
+    listItem.remove();
+  });
+
+  listItem.appendChild(span);
+  listItem.appendChild(button);
+  return listItem;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.querySelector('button').addEventListener('click', function (event) {
+    let inputBox = document.getElementById('item');
+    createNewListItem(inputBox.value);
+    let li = createNewListItem(inputBox.value);
+    let ul = document.querySelector('ul');
+    ul.appendChild(li);
+
+    //call createNewListItem with the contents of the input widget and save
+    //the result in variable
+
+  });
+    // Add code here that listen for 'keyup' on the 'input' element
+   //and logs the event.key property
+  document.querySelector('input').addEventListener('keyup',function (event){
+    if (event.key === 'Enter'){
+      console.log(event.key);
+      let inputBox = document.getElementById('item');
+      let li = createNewListItem(inputBox.value);
+      document.querySelector('ul').appendChild(li);
+    }
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
