d2916e698818c348d1598452f111a9b10cb8dc89
Helen Berhane <helen.berhane@powercoders.org>
2019-01-11 15:56:49 +0100

    Disable the add item button after clicking and pressing the enter-key

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -25,26 +25,33 @@ function createNewListItem(itemName) {
 document.addEventListener('DOMContentLoaded', function (event) {
   let inputBox = document.getElementById('item');
   let shoppingList = document.querySelector('ul');
+  let addItemButton = document.querySelector('button');
 
   document.querySelector('button').addEventListener('click', function (event) {
     if (inputBox.value.trim()!== '') {
       shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
       inputBox.value = '';
+      addItemButton.disabled = true;
     }
     inputBox.focus();
   });
 
   inputBox.addEventListener('keyup', function (event) {
     if(inputBox.value.trim() !== ''){
+      addItemButton.disabled = false;
     if (event.key === 'Enter'){
         shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
         inputBox.value = '';
-
       }
+    }
+
+    if (inputBox.value.trim() === ''){
+      addItemButton.disabled = true;
     }
   });
 
   inputBox.focus();
+  addItemButton.disabled = true;
 });
 
 
